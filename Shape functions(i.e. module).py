63c4a64b9767b276e5d2e8bc91b9b8c8342aefaa
ShashCode2348 <96688819+ShashCode2348@users.noreply.github.com>
2022-01-21 20:53:31 +0000

    Add files via upload

---
+++ Shape functions(i.e. module).py
@@ -0,0 +1,123 @@
+class turtleShapes:
+    import turtle as t
+    def rectangle_with_fill(horizontal, vertical, color):
+        t.pendown()
+        t.pensize(1)
+        t.color(color)
+        t.begin_fill()
+        for counter in range(2):
+            t.forward(horizontal)
+            t.right(90)
+            t.forward(vertical)
+            t.right(90)
+        t.end_fill()
+        t.penup()
+
+    def star_with_fill(length, points, color):
+        sumangle = ((points * 2) - 2) * 180
+        oneangle = sumangle/points
+        smallangle = oneangle/3.5
+        bigangle = oneangle - smallangle
+        t.pensize(1)
+        t.color(color)
+        t.begin_fill()
+        t.pendown()
+        for counter in range(points):
+            t.forward(length)
+            t.left(smallangle)
+            t.forward(length)
+            t.left(bigangle)
+        t.end_fill()
+        t.penup()
+
+    def star_with_fill_improved(length, points, color):
+        t.penup()
+        angle = 180 - (180/points)
+        t.color(color)
+        t.begin_fill()
+        for point in range(points):
+            t.forward(length)
+            t.right(angle)
+        t.end_fill()
+        t.penup()
+
+    def any_polygon_with_fill(length, angles, color):
+        sumangle = (angles - 2) * 180
+        oneangle = (sumangle/angles) - 180
+        t.color(color)
+        t.pensize(1)
+        t.begin_fill()
+        t.pendown()
+        for counter in range(angles):
+            t.forward(length)
+            t.right(oneangle)
+        t.end_fill()
+        t.penup()
+
+    def rectangle_without_fill(horizontal, vertical, color, pensize):
+        t.pendown()
+        t.pensize(pensize)
+        t.color(color)
+        for counter in range(2):
+            t.forward(horizontal)
+            t.right(90)
+            t.forward(vertical)
+            t.right(90)
+        t.penup()
+
+    def star_without_fill(length, points, color, pensize):
+        sumangle = ((points * 2) - 2) * 180
+        oneangle = sumangle/points
+        smallangle = oneangle/3.5
+        bigangle = oneangle - smallangle
+        t.color(color)
+        t.pensize(pensize)
+        t.pendown()
+        for counter in range(points):
+            t.forward(length)
+            t.left(smallangle)
+            t.forward(length)
+            t.left(bigangle)
+        t.penup()
+
+    def star_without_fill_improved(length, points, color, pensize):
+        t.pendown()
+        angle = 180 - (180/points)
+        t.color(color)
+        t.pensize(pensize*2)
+        for point in range(points):
+            t.forward(length)
+            t.right(angle)
+        t.penup()
+        t.right((angle/4))
+        t.forward(pensize/4)
+        t.setheading(0)
+        t.pensize(1)
+        t.forward(pensize/4)
+        t.color('#FFFFFF')
+        t.pendown()
+        t.begin_fill()
+        for point in range(points):
+            t.forward(length - (pensize))
+            t.right(angle)
+        t.end_fill()
+        t.penup()
+
+    def any_polygon_without_fill(length, angles, color, pensize):
+        sumangle = (angles - 2) * 180
+        oneangle = (sumangle/angles) - 180
+        t.color(color)
+        t.pensize(pensize)
+        t.pendown()
+        for counter in range(angles):
+            t.forward(length)
+            t.right(oneangle)
+        t.penup()
+
+
+
+
+
+
+
+
